1023601d4644ec938c6a9d233d27de96037f5b76
Kurohiko2004 <vietanh.nguyengia2004@gmail.com>
2026-04-14 09:43:21 +0700

    feat(backend): cart validation - glassId, quantity validation (POST, PATCH)

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
 const { protect } = require('../middlewares/authMiddleware');
+const validate = require('../middlewares/validateMiddleware');
+const { addToCartSchema, updateQuantitySchema } = require('../validations/cartValidation');
 
 // Tất cả các route giỏ hàng đều yêu cầu đăng nhập
 router.use(protect);
 
-router.post('/add', cartController.addToCart);
+router.post('/add', validate(addToCartSchema, 'body'), cartController.addToCart);
 router.get('/', cartController.getMyCart);
-router.patch('/:id', cartController.updateQuantity);
+router.patch('/:id', validate(updateQuantitySchema, 'body'), cartController.updateQuantity);
 router.delete('/:id', cartController.removeFromCart);
 
 module.exports = router;
